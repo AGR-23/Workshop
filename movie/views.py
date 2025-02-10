e9bf28873327e28ece4a0870773015c25f34211f
agr <agarcesr@eafit.edu.co>
2025-02-10 13:54:32 -0500

    Adding movies into the database and editing views.py

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Alejandro Garcés Ramírez'})
+    #return render(request, 'home.html', {'name':'Alejandro Garcés Ramírez'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page<h1>')
+    return render(request, 'about.html')
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.}
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
