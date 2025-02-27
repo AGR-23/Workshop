e500f97928dcea561f8d3f6898ac4c0e508addb1
agr <agarcesr@eafit.edu.co>
2025-02-27 10:55:00 -0500

    Adding the path 'news' with non-fictional news

--- news/views.py
+++ news/views.py
@@ -3,6 +3,6 @@ from .models import News
 
 def news(request):
     newss = News.objects.all().order_by('-date')
-    return render(request, 'news.html', {newss})
+    return render(request, 'news.html', {'newss':newss})
 
 # Create your views here.
